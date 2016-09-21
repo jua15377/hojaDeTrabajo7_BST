d9677c9f672478c6ba0aa0aa224ad399bd8275cb
JonnathanJuarez <jua15377@uvg.edu.gt>
2016-09-21 11:33:25 -0600

    Agregando Main

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+/**
+ * Clase principal,
+ * @author Eric Mendoza 15002
+ * @author Jonnathan Juarez 15377
+ * @since 21/09/2016
+ * @version 1.0
+ */
+
+public class Main{
+    public static void main(String[] args){
+
+    }
+}
