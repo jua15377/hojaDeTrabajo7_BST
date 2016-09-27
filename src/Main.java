7b40f15b27ae7a9d51d40f274a7d87df94d59ed4
JonnathanJuarez <jua15377@uvg.edu.gt>
2016-09-27 01:01:51 -0600

    se puede seleccionar el diccionario

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,9 @@
+import javax.swing.*;
+import javax.swing.filechooser.FileNameExtensionFilter;
+import java.io.*;
+import java.util.Scanner;
+import java.util.*;
+
 /**
  * Clase principal,
  * @author Eric Mendoza 15002
@@ -6,8 +12,43 @@
  * @version 1.0
  */
 
-public class Main{
-    public static void main(String[] args){
+public class Main {
+    public static void main(String[] args) throws IOException {
+        Scanner ingreso = new Scanner(System.in);
+        ArrayList<String> diccionario;
+        diccionario = new ArrayList<String>();
 
+        System.out.println("Bienvenido!\nSeleccione el archivo .TXT que contiene el Diccionario\n(presiona ENTER para continuar)");
+        ingreso.nextLine();
+        /*
+         codigo tomado de :
+         http://stackoverflow.com/questions/7494478/jfilechooser-from-a-command-line-program-and-popping-up-underneath-all-windows
+         */
+        JFileChooser chooser = new JFileChooser();
+        chooser.setCurrentDirectory(new java.io.File("./src"));
+        chooser.setDialogTitle("Seleccione su archivo");
+        chooser.setFileFilter(new FileNameExtensionFilter("Text files (.txt)", "txt"));
+        int returnVal = chooser.showOpenDialog(null);
+        if (returnVal == JFileChooser.APPROVE_OPTION) {
+
+            try {
+						/*Lee el archivo y obtiene la cadena*/
+                FileInputStream fstream = new FileInputStream(chooser.getSelectedFile().getAbsolutePath());
+                BufferedReader br = new BufferedReader(new InputStreamReader(fstream));
+
+                String strLine;
+                while ((strLine = br.readLine()) != null) {
+                    diccionario.add(strLine); //agregar cada linea al diccionario
+                }
+
+                /* Separar cadenas y hacer asociacion */
+                for (int i = 0; i < diccionario.size(); i++) {
+                    String temp = diccionario.get(i).substring(1, diccionario.get(i).length() - 1);
+                    String[] partes = temp.split(", ");
+                }
+            } catch (Exception e) {
+                System.out.println("Archivo no valido!!!");
+            }
+        }
     }
 }
