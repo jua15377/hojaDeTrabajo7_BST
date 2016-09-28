68672bd8e092a91ff1ab7908b39f562763cdb529
JonnathanJuarez <jua15377@uvg.edu.gt>
2016-09-27 19:17:33 -0600

    traducctor funcional, se debe de compilat por medio de consila de windows

--- src/Main.java
+++ src/Main.java
@@ -15,8 +15,11 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) throws IOException {
         Scanner ingreso = new Scanner(System.in);
+        String source = "";
         ArrayList<String> diccionario;
         diccionario = new ArrayList<String>();
+        BinaryTree<String, String> dic  = new BinaryTree<String, String>();
+        String textoTraducido = "";
 
         System.out.println("Bienvenido!\nSeleccione el archivo .TXT que contiene el Diccionario\n(presiona ENTER para continuar)");
         ingreso.nextLine();
@@ -45,10 +48,69 @@ public class Main {
                 for (int i = 0; i < diccionario.size(); i++) {
                     String temp = diccionario.get(i).substring(1, diccionario.get(i).length() - 1);
                     String[] partes = temp.split(", ");
+                    dic.insert(partes[0],partes[1]);
                 }
             } catch (Exception e) {
                 System.out.println("Archivo no valido!!!");
             }
         }
+
+        /*abrir el texto*/
+        System.out.println("Seleccione el archivo .TXT que contiene el texto a traducir\n(presiona ENTER para continuar)");
+        ingreso.nextLine();
+        JFileChooser chooser2 = new JFileChooser(System.getProperty("java.class.path"));
+        chooser2.setDialogTitle("Seleccione su archivo");
+        chooser2.setFileFilter(new FileNameExtensionFilter("Text files (.txt)", "txt"));
+        int returnVal2 = chooser2.showOpenDialog(null);
+        if(returnVal2 == JFileChooser.APPROVE_OPTION) {
+            try {
+                Scanner inputFile2 = new Scanner(new File(chooser2.getSelectedFile().getAbsolutePath()));
+                source = chooser2.getSelectedFile().getAbsolutePath();
+            }
+            catch (FileNotFoundException e) {
+                System.out.println("No se sseleciono ningun archivo");
+
+            }
+        }
+
+
+        try
+        {
+            BufferedReader datos = new BufferedReader(new FileReader(source));  // Abre documento para su lectura
+            String rawText = datos.readLine();  // Lee la unica linea y la guarda como string
+            /*en caso de punto al final*/
+            rawText = rawText.substring(0, rawText.length()-1);
+            String textoATraducir = rawText.toLowerCase();
+            datos.close();  // Se finaliza el lector
+            System.out.println("El texto Originas es: \n" + textoATraducir);
+            String wIngles, wEspanol;
+            StringTokenizer st = new StringTokenizer (textoATraducir);
+
+            /* traduce las palabras*/
+            while (st.hasMoreTokens())
+            {
+                wIngles = st.nextToken();
+                wEspanol = dic.find(wIngles);
+                if (wEspanol != null){
+                    textoTraducido = (textoTraducido +" "+ wEspanol);
+                }
+                else{
+                    textoTraducido = (textoTraducido + " *"+wIngles+"*");
+                }
+
+            }
+            System.out.println("El texto traducido es: \n");
+            textoTraducido = textoTraducido + ".";
+            System.out.println(textoTraducido);
+        }
+        catch(FileNotFoundException e)
+        {
+            System.out.println("Archivo no valido!!!");
+        }
+        System.out.println("\nSi la palabra se encuentra entre  asteriscos (**), no existe en el diccionario\n");
+        System.out.println("Palabras dentro del diccionario utlizado (inOrder): \n");
+        dic.display(dic.root);
+
+
     }
 }
